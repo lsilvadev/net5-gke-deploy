cdc6a58b211cdc8a0ad1eec7c58cc607a90026fe
Leticia Silva <leticia.silva@acessodigital.com.br>
2022-05-22 15:30:49 -0300

    Added cs project and github actions pipeline

---
+++ Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class TestController : ControllerBase
+	{
+		[HttpGet]
+		public string Get()
+		{
+			return ("ok");
+		}
+	}
+}
