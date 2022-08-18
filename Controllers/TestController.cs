9037529696893c51ecde4078005b636f55b20e18
Leticia Silva <lsilva@C02G59C6ML7H.local>
2022-08-18 01:00:22 -0300

    Project files

---
+++ Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class TestController : ControllerBase
+	{
+		[HttpGet]
+		public string Get()
+		{
+			return ("Ok (1)");
+		}
+	}
+}
